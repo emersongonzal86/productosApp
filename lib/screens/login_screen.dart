f9c411234a1c80212603993281764a0a2090b66b
emerson <emersongonzal86@gmail.com>
2023-11-11 19:19:21 -0500

    White container

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,12 +6,24 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(body: AuthBackground(
-      child: Container(
-        width: double.infinity,
-        height: 300,
-        color: Colors.red,
-      ),
-    ));
+    return Scaffold(
+        body: AuthBackground(
+            child: SingleChildScrollView(
+                child: Column(
+      children: [
+        const SizedBox(height: 250),
+        CardContainer(
+          child: Column(children: [
+            const SizedBox(height: 10),
+            Text(
+              'Login',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 30),
+            Text('Formulario')
+          ]),
+        ),
+      ],
+    ))));
   }
 }
