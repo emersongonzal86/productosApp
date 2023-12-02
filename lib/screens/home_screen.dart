742b610a1a9fab66e27628d4b68c213814635ff0
emerson <emersongonzal86@gmail.com>
2023-12-02 14:08:29 -0500

    Upload products from firebase

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:productos_app/services/services.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,6 +9,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productsService = Provider.of<ProductsService>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Productos'),
@@ -13,10 +18,8 @@ class HomeScreen extends StatelessWidget {
       body: ListView.builder(
           itemCount: 10,
           itemBuilder: (BuildContext context, int index) => GestureDetector(
-            onTap: () => Navigator.pushNamed(context,'product'),
-            child: ProductCard()
-            )
-            ),
+              onTap: () => Navigator.pushNamed(context, 'product'),
+              child: ProductCard())),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {},
--- lib/services/products_services.dart
+++ lib/services/products_services.dart
@@ -1,9 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:productos_app/models/models.dart';
+import 'package:http/http.dart' as http;
 
 class ProductsService extends ChangeNotifier {
   final String _baseUrl = 'flutter-varios-a20a2-default-rtdb.firebaseio.com';
   final List<Product> products = [];
+  //para saber si esta cargando los datos inicia en true porque inicia cargando datos
+  //no se coloca final por que va esta variando entre true y false
+  bool isLoading = true;
 
-  //TODO: hacer fetch de productos
+  ProductsService(){
+    this.loadProducts();
+  }
+
+//TODO: <List<Product>>
+  Future loadProducts() async {
+
+    final url = Uri.https( _baseUrl, 'products.json');
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> productsMap = json.decode(resp.body);
+
+   //cargamos el mapa como lista
+
+   productsMap.forEach((key, value) {
+    final tempProduct = Product.fromMap(value);
+    tempProduct.id = key;
+    this.products.add( tempProduct );
+   });
+
+   print(this.products[0].name);
+
+  }
+
+
+  
 }
