ea023f4d604bbd0e743aa4c0c738c049248446fe
emerson <emersongonzal86@gmail.com>
2023-12-01 19:08:27 -0500

    Widget - ProductCard - part 2

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -14,7 +14,14 @@ class ProductCard extends StatelessWidget {
         decoration: _cardBorders(),
         child: Stack(
           alignment: Alignment.bottomLeft,
-          children: [_BackgroundImage(), _ProductDetails()],
+          children: [
+            _BackgroundImage(),
+            _ProductDetails(),
+            Positioned(top: 0, right: 0, child: _PriceTag()),
+
+            //TODO: Mostrar de manera condicional
+            Positioned(top: 0, left: 0, child: _NotAvailable())
+          ],
         ),
       ),
     );
@@ -29,6 +36,57 @@ class ProductCard extends StatelessWidget {
           ]);
 }
 
+class _NotAvailable extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            "No disponible",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+      width: 100,
+      height: 70,
+      decoration: BoxDecoration(
+          color: Colors.yellow[800],
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25), bottomRight: Radius.circular(25))),
+    );
+  }
+}
+
+class _PriceTag extends StatelessWidget {
+  const _PriceTag({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 70,
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+          color: Colors.indigo,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(25), bottomLeft: Radius.circular(25))),
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: const Text('\$103.99',
+              style: TextStyle(color: Colors.white, fontSize: 20)),
+        ),
+      ),
+    );
+  }
+}
+
 class _ProductDetails extends StatelessWidget {
   const _ProductDetails({
     super.key,
@@ -36,19 +94,45 @@ class _ProductDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 70,
-      color: Colors.indigo,
+    return Padding(
+      padding: const EdgeInsets.only(right: 50),
+      child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          width: double.infinity,
+          height: 70,
+          decoration: _buildBoxDecoration(),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Disco duro G',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                'Id del disco duro',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+                overflow: TextOverflow.ellipsis,
+              )
+            ],
+          )),
     );
   }
+
+  BoxDecoration _buildBoxDecoration() => const BoxDecoration(
+      color: Colors.indigo,
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25), topRight: Radius.circular(25)));
 }
 
 class _BackgroundImage extends StatelessWidget {
-  const _BackgroundImage({
-    super.key,
-  });
-
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
@@ -56,7 +140,7 @@ class _BackgroundImage extends StatelessWidget {
       child: Container(
         width: double.infinity,
         height: 400,
-        child: FadeInImage(
+        child: const FadeInImage(
           placeholder: AssetImage('assets/jar-loading.gif'),
           image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
           fit: BoxFit.cover,
