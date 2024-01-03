0834e9d28f8f79ce0b94318aa83970724f35739d
emerson <emersongonzal86@gmail.com>
2024-01-02 20:39:53 -0500

    Eliminate transition between screens

--- lib/screens/check_auth_screen.dart
+++ lib/screens/check_auth_screen.dart
@@ -1,33 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'package:productos_app/services/services.dart';
+import 'package:productos_app/screens/screens.dart';
 
+import 'package:productos_app/services/services.dart';
 
 class CheckAuthScreen extends StatelessWidget {
   const CheckAuthScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    final authService= Provider.of<AuthService>(context, listen: false);
-
+    final authService = Provider.of<AuthService>(context, listen: false);
 
     return Scaffold(
       body: Center(
         child: FutureBuilder(
           future: authService.readToken(),
-          
           builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-            if( !snapshot.hasData )
-            return Text('Espere....') ;
+            if (!snapshot.hasData) return Text('');
 
-            if( snapshot.data == ''){
+            if (snapshot.data == '') {
+              Future.microtask(() {
+              Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                      pageBuilder: (_, __, ___) => LoginScreen(),
+                      transitionDuration: Duration(seconds: 0)));
 
-            }
-            Future.microtask(() {
-                Navigator.of(context).pushReplacementNamed('home');
+              //Navigator.of(context).pushReplacementNamed('login');
             });
+            }else{
+              Future.microtask(() {
+              Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                      pageBuilder: (_, __, ___) => HomeScreen(),
+                      transitionDuration: Duration(seconds: 0)));
+
+              //Navigator.of(context).pushReplacementNamed('login');
+            });
+            }
             
 
             return Container();
